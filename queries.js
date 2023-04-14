e9790c83b1653390a8a04b986c0bebc3dbbe5a8d
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-13 23:57:24 -0700

    Define other methods such as delete a role. delete an employees and retrieve roles by a specific department

--- queries.js
+++ queries.js
@@ -119,6 +119,119 @@ async addRole(title, salary, departmentId) {
 }
 
 
+// Define a method to add a new employee to the database
+async addEmployee(firstName, lastName, roleId, managerId) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("INSERT INTO employee SET ?", {
+    first_name: firstName,
+    last_name: lastName,
+    role_id: roleId,
+    manager_id: managerId,
+  });
+  connection.end();
+
+  console.log("Employee added successfully.");
+  return result;
+}
+// Define a method to update an employee's role in the database
+async updateEmployeeRole(employeeId, newRoleId) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [newRoleId, employeeId]);
+  connection.end();
+
+  console.log("Employee role updated successfully.");
+  return result;
+}
+// Define a method to update an employee's manager in the database
+async updateEmployeeManager(employeeId, newManagerId) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("UPDATE employee SET manager_id = ? WHERE id = ?", [newManagerId, employeeId]);
+  connection.end();
+
+  console.log("Employee manager updated successfully.");
+  return result;
+}
+// Define a method to retrieve all departments from the database
+async getDepartments() {
+  const connection = await this.getConnection();
+  const [rows] = await connection.query("SELECT * FROM department");
+  connection.end();
+
+  return rows;
+}
+// Define a method to retrieve all roles from the database
+async getRoles() {
+  const connection = await this.getConnection();
+  const [rows] = await connection.query("SELECT * FROM role");
+  connection.end();
+
+  return rows;
+}
+// Define a method to retrieve all employees from the database
+async getEmployees() {
+  const connection = await this.getConnection();
+  const [rows] = await connection.query("SELECT * FROM employee");
+  connection.end();
+
+  return rows;
+}
+
+// Define a method to retrieve all managers from the database
+async getManagers() {
+  const connection = await this.getConnection();
+  const [rows] = await connection.query(
+    "SELECT DISTINCT m.id, m.first_name, m.last_name FROM employee e JOIN employee m ON e.manager_id = m.id"
+  );
+  connection.end();
+
+  return rows;
+}
+
+// Define a method to delete a department from the database
+async deleteDepartment(id) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("DELETE FROM department WHERE id = ?", [id]);
+  connection.end();
+
+  console.log("Department deleted successfully.");
+  return result;
+}
+
+// Define a method to retrieve roles by a specific department from the database
+async getRolesByDepartment(departmentId) {
+  const connection = await this.getConnection();
+  const [rows] = await connection.query("SELECT * FROM role WHERE department_id = ?", [departmentId]);
+  connection.end();
+
+  return rows;
+}
+
+// Define a method to delete a role from the database
+async deleteRole(id) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("DELETE FROM role WHERE id = ?", [id]);
+  connection.end();
+
+  console.log("Role deleted successfully.");
+  return result;
+}
+
+// Define a method to delete an employee from the database
+async deleteEmployee(id) {
+  const connection = await this.getConnection();
+  const [result] = await connection.query("DELETE FROM employee WHERE id = ?", [id]);
+  connection.end();
+
+  console.log("Employee deleted successfully.");
+  return result;
+}
+}
+
+// Export the Queries class for use in other modules
+module.exports = Queries;
+
+
+
 
 
 
