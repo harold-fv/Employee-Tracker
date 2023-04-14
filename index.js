70b902c9a0b0c7f11f54ee20e4dc0cd379822b2d
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-13 23:35:25 -0700

    Define mainMenu Function in index.js

--- index.js
+++ index.js
@@ -15,3 +15,86 @@ const config = {
 // Initialize a new Queries object with the given configuration
 // to perform database operations
 const queries = new Queries(config);
+
+
+// Define the mainMenu function which prompts the user for
+// an action and executes the corresponding function
+async function mainMenu() {
+  const { action } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View departments",
+        "View roles",
+        "View employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update employee role",
+        "Update employee manager",
+        "View employees by manager",
+        "View employees by department",
+        "Delete departments",
+        "Delete roles",
+        "Delete employees",
+        "View department budget",
+        "Exit",
+      ],
+    },
+  ]);
+
+ // Call the corresponding function based on the selected action
+  switch (action) {
+    case "View departments":
+      await queries.viewDepartments();
+      mainMenu();
+      break;
+    case "View roles":
+      await queries.viewRoles();
+      mainMenu();
+      break;
+    case "View employees":
+      await queries.viewEmployees();
+      mainMenu();
+      break;
+    case "Add a department":
+      promptAddDepartment();
+      break;
+    case "Add a role":
+      promptAddRole();
+      break;
+    case "Add an employee":
+      promptAddEmployee();
+      break;
+    case "Update employee role":
+      promptUpdateEmployeeRole();
+      break;
+    case "Update employee manager":
+      promptUpdateEmployeeManager();
+      break;
+    case "View employees by manager":
+      promptViewEmployeesByManager();
+      break;
+    case "View employees by department":
+      promptViewEmployeesByDepartment();
+      break;
+    case "Delete departments":
+      promptDeleteDepartment();
+      break;
+    case "Delete roles":
+      promptDeleteRole();
+      break;
+    case "Delete employees":
+      promptDeleteEmployee();
+      break;
+    case "View department budget":
+      promptViewDepartmentBudget();
+      break;
+    case "Exit":
+      process.exit();
+      break;
+  }
+}
+
