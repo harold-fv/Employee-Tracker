355c4a2cd7610fa04382439bc7aec1e89909f960
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-13 23:52:03 -0700

    Define a method to view all employees in the database

--- queries.js
+++ queries.js
@@ -49,5 +49,24 @@ async viewDepartmentBudget(departmentId) {
     return rows;
   }
 
-
   
+    // Define a method to view all employees in the database
+    async viewEmployees() {
+      const connection = await this.getConnection();
+      const [rows] = await connection.query(
+        "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id"
+      );
+      connection.end();
+  
+      console.table(rows);
+      return rows;
+    }
+
+    
+    
+
+
+
+
+
+
